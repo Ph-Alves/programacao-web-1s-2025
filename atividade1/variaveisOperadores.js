238f08c4943c49283a0d3f6fb216301be7b9f525
Ph-Alves <ph.costaalves@gmail.com>
2025-02-25 11:08:12 -0300

    Exercício 1

--- atividade1/variaveisOperadores.js
+++ atividade1/variaveisOperadores.js
@@ -1,22 +1,22 @@
 let num_A = 2
 let num_B = 4
 
-let result = num1 + num2
+let result = num_A + num_B
 let operacao = 'soma'
 console.log(`Resultado da ${operacao} entre ${num_A} e ${num_B} é ${resultado}`)
 
-resultado = num1 - num2
+resultado = num_A - num_B
 operacao = 'subtração'
 console.log(`Resultado da ${operacao} entre ${num_A} e ${num_B} é ${resultado}`)
 
-result = num1 * num2
+result = num_A * num_B
 operacao = 'multiplicação'
 console.log(`Resultado da ${operacao} entre ${num_A} e ${num_B} é ${resultado}`)
 
-result = num1 / num2
+result = num_A / num_B
 operacao = 'divisão'
 console.log(`Resultado da ${operacao} entre ${num_A} e ${num_B} é ${resultado}`)
 
-result = num1 % num2
+result = num_A % num_B
 operacao = 'divisão de resto'
-console.log(result)
+console.log(`Resultado da ${operacao} entre ${num_A} e ${num_B} é ${resultado}`)
