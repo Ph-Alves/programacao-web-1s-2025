12c5793a1e1cd644c2772c49eb8c0566830c1804
Ph-Alves <ph.costaalves@gmail.com>
2025-02-25 10:54:40 -0300

    variáveis do exercicio 1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,22 @@
+let num_A = 2
+let num_B = 4
+
+let result = num1 + num2
+let operacao = 'soma'
+console.log(`Resultado da ${operacao} entre ${num_A} e ${num_B} é ${resultado}`)
+
+resultado = num1 - num2
+operacao = 'subtração'
+console.log(`Resultado da ${operacao} entre ${num_A} e ${num_B} é ${resultado}`)
+
+result = num1 * num2
+operacao = 'multiplicação'
+console.log(`Resultado da ${operacao} entre ${num_A} e ${num_B} é ${resultado}`)
+
+result = num1 / num2
+operacao = 'divisão'
+console.log(`Resultado da ${operacao} entre ${num_A} e ${num_B} é ${resultado}`)
+
+result = num1 % num2
+operacao = 'divisão de resto'
+console.log(result)
