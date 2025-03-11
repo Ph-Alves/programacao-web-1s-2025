b75dde83909da4f6470ffba966fab95607064128
Ph-Alves <ph.costaalves@gmail.com>
2025-03-11 11:02:05 -0300

    Início da atividade 2

---
+++ atividade2/verificarNumeroPrimo.js
@@ -0,0 +1,17 @@
+function verificarNumeroPrimo(n){
+    if (n % 1 == 0 || n % n == 0){
+        console.log('true')
+    }
+    console.log('false')
+}
+
+verificarNumeroPrimo(0); //false
+verificarNumeroPrimo(1); //false
+verificarNumeroPrimo(2); //true
+verificarNumeroPrimo(3); //true
+verificarNumeroPrimo(7); //true
+verificarNumeroPrimo(83); //true
+verificarNumeroPrimo(100); //false
+verificarNumeroPrimo(991); //true
+verificarNumeroPrimo(104729); //true
+verificarNumeroPrimo(14348907); //false
