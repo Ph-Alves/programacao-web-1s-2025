665ab9a3ee224fcca2429e880d591d6d305eb934
PAULO HENRIQUE COSTA ALVES <paulo.halves@a.ucb.br>
2025-04-01 10:38:17 -0300

    Base da atividade 4

---
+++ atividade4/index.js
@@ -0,0 +1,24 @@
+const estoque = require('.util/estoque')
+const express = require('express')
+
+const app = express()
+
+app.get("/", (req, res)=> {
+
+})
+
+app.get("/adicionar/:id/:nome/:id", (req, res)=>{
+
+})
+
+app.get("/listar", (req, res)=>{
+    
+})
+
+app.get("/remover/:id", (req, res)=>{
+    
+})
+
+app.get("/editar/:id/:qntd", (req, res)=>{
+    
+})
---
+++ atividade4/util/estoque.js
@@ -0,0 +1,13 @@
+function adicionar(id, nome, qntd, estoque){
+
+}
+
+function listar(estoque){
+
+}
+
+function remover(estoque, id){
+
+}
+
+function editar(id, qntd)
